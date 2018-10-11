ec2e7bf291b14c4fbd8b4ded561dd02c58f36edb
Artur Kosałka <kosial@tlen.pl>
2018-10-11 21:51:58 +0200

    Adding Test tables

---
+++ ContosoDataBase/ContosoDataBase/dbo/Tables/Test_2.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Test_2] (
+    [ID]               INT             NOT NULL,
+    [NazwaProduktu]    VARCHAR (45)    NOT NULL,
+    [RodzajProduktuID] INT             NOT NULL,
+    [CenaNetto]        DECIMAL (10, 2) NOT NULL,
+    [CenaBrutto]       DECIMAL (10, 2) NOT NULL,
+    [Opis]             NVARCHAR (1000) NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC),
+    FOREIGN KEY ([RodzajProduktuID]) REFERENCES [dbo].[Test_1] ([ID])
+);
+
---
+++ ContosoDataBase/ContosoDataBase/dbo/Tables/Test_3.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Test_3] (
+    [ID]          INT             IDENTITY (1, 1) NOT NULL,
+    [Nazwa]       NVARCHAR (500)  NOT NULL,
+    [NIP]         NVARCHAR (20)   NOT NULL,
+    [KodPocztowy] NVARCHAR (20)   NULL,
+    [Miasto]      NVARCHAR (250)  NULL,
+    [Adres]       NVARCHAR (1000) NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
